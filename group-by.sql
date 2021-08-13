be8048a0efc0b7bc455b3f5ef566e8dc23a66964
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-12 22:36:06 -0700

    Finished intermediate portion

--- group-by.sql
+++ group-by.sql
@@ -1,1 +1,21 @@
 -- 1
+SELECT COUNT(track_id), g.name
+FROM track t
+JOIN genre g
+ON t.genre_id = g.genre_id
+GROUP BY g.name
+
+-- 2
+SELECT COUNT(*), g.name
+FROM track t
+JOIN genre g
+ON t.genre_id = g.genre_id
+WHERE g.name = 'Rock' OR g.name = 'Pop'
+GROUP BY g.name
+
+-- 3
+SELECT COUNT(al.album_id), ar.name
+FROM artist ar
+JOIN album al
+ON ar.artist_id = al.artist_id
+GROUP BY ar.name
